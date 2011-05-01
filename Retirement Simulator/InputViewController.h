9b0cd16584f7bb26b508bc71e7f9b377d2a71447
Steve Roehling <sroehling@Roehlings-Computer-2.local>
2011-05-01 07:16:38 -0700

    Implement a initial skeleton of an input view.
    Incorporate a navigation controller within the tab bar, add an InputViewController class, and and initialize the navigation toolbar with an add button.

---
+++ Retirement Simulator/InputViewController.h
@@ -0,0 +1,16 @@
+//
+//  InputViewController.h
+//  Retirement Simulator
+//
+//  Created by Steve Roehling on 5/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface InputViewController : UIViewController {
+    
+}
+
+@end
