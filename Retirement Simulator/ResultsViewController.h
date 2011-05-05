e5cd934ccb59bb867707201a902127b35f1aa134
Steve Roehling <sroehling@Roehlings-Computer-2.local>
2011-05-05 10:55:52 -0700

    Remove placeholder views which were automatically created with the new project, and replace them with an initial results view. Also added groups to the project and reorganized the source files into these groups.

---
+++ Retirement Simulator/ResultsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ResultsViewController.h
+//  Retirement Simulator
+//
+//  Created by Steve Roehling on 5/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ResultsViewController : UIViewController {
+    
+}
+
+@end
