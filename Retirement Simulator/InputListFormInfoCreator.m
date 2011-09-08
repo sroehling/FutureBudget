c481f81c259195f086223c7fc10b60f520e76a6b
Steve Roehling <sroehling@Roehlings-Computer-2.local>
2011-09-08 12:15:53 -0700

    Restore grouping in the inputs list.

--- Retirement Simulator/InputListFormInfoCreator.m
+++ Retirement Simulator/InputListFormInfoCreator.m
@@ -15,8 +15,11 @@
 #import "InputListObjectAdder.h"
 #import "TableHeaderWithDisclosure.h"
 #import "SharedAppValues.h"
+#import "IncomeInput.h"
+#import "ExpenseInput.h"
 #import "Scenario.h"
 #import "LocalizationHelper.h"
+#import "Account.h"
 #import "SelectScenarioTableHeaderButtonDelegate.h"
 
 @implementation InputListFormInfoCreator
@@ -40,17 +43,57 @@
 	[tableHeader resizeForChildren];
 	formPopulator.formInfo.headerView = tableHeader;
 	
-	SectionInfo *sectionInfo = [formPopulator nextSection];
-
+	SectionInfo *sectionInfo;
+	
 	NSArray *inputs = [[DataModelController theDataModelController]
-								   fetchSortedObjectsWithEntityName:@"Input" sortKey:@"name"];
-	for (Input *input in inputs)
+			fetchSortedObjectsWithEntityName:INCOME_INPUT_ENTITY_NAME sortKey:INPUT_NAME_KEY];
+	if([inputs count] > 0)
 	{
-		// Create the row information for the given milestone date.
-		InputFieldEditInfo *inputFieldEditInfo =
-			[[[InputFieldEditInfo alloc] initWithInput:input] autorelease];
-		[sectionInfo addFieldEditInfo:inputFieldEditInfo];
+		sectionInfo = [formPopulator nextSection];
+		sectionInfo.title = LOCALIZED_STR(@"INPUT_LIST_SECTION_TITLE_INCOMES");
+		for(IncomeInput *income in inputs)
+		{    
+			assert(income != nil);
+			InputFieldEditInfo *inputFieldEditInfo =
+				[[[InputFieldEditInfo alloc] initWithInput:income] autorelease];
+			[sectionInfo addFieldEditInfo:inputFieldEditInfo];
+		}
 	}
+
+
+	inputs = [[DataModelController theDataModelController]
+			fetchSortedObjectsWithEntityName:EXPENSE_INPUT_ENTITY_NAME sortKey:INPUT_NAME_KEY];
+	if([inputs count] > 0)
+	{
+		sectionInfo = [formPopulator nextSection];
+		sectionInfo.title = LOCALIZED_STR(@"INPUT_LIST_SECTION_TITLE_EXPENSES");
+		for(ExpenseInput *expense in inputs)
+		{    
+			assert(expense != nil);
+			InputFieldEditInfo *inputFieldEditInfo =
+				[[[InputFieldEditInfo alloc] initWithInput:expense] autorelease];
+			[sectionInfo addFieldEditInfo:inputFieldEditInfo];
+		}
+	}
+
+
+
+	inputs = [[DataModelController theDataModelController]
+			fetchSortedObjectsWithEntityName:ACCOUNT_ENTITY_NAME sortKey:INPUT_NAME_KEY];
+	if([inputs count] > 0)
+	{
+		sectionInfo = [formPopulator nextSection];
+		sectionInfo.title = LOCALIZED_STR(@"INPUT_LIST_SECTION_TITLE_ACCOUNTS");
+		for(Account *account in inputs)
+		{    
+			assert(account != nil);
+			InputFieldEditInfo *inputFieldEditInfo =
+				[[[InputFieldEditInfo alloc] initWithInput:account] autorelease];
+			[sectionInfo addFieldEditInfo:inputFieldEditInfo];
+		}
+	}
+
+
 	return formPopulator.formInfo;
 	
 }
