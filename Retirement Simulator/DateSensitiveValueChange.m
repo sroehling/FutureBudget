5c10a500596534024b8c4f48113f3020137450a7
Steve Roehling <sroehling@resultra.com>
2013-09-06 15:11:45 -0700

    Fix a constant used for a key-value property lookup. The DateSensitiveValueChange property name changed, but not the constant.

--- Retirement Simulator/DateSensitiveValueChange.m
+++ Retirement Simulator/DateSensitiveValueChange.m
@@ -12,7 +12,7 @@
 NSString * const DATE_SENSITIVE_VALUE_CHANGE_START_DATE_SORT_KEY = @"resolvedStartDate";
 NSString * const DATE_SENSITIVE_VALUE_CHANGE_ENTITY_NAME = @"DateSensitiveValueChange";
 NSString * const DATE_SENSITIVE_VALUE_CHANGE_START_DATE_KEY = @"startDate";
-NSString * const DATE_SENSITIVE_VALUE_CHANGE_NEW_VALUE_KEY = @"newValue";
+NSString * const DATE_SENSITIVE_VALUE_CHANGE_NEW_VALUE_KEY = @"valueAfterChange";
 
 @implementation DateSensitiveValueChange
 @dynamic startDate;
