1b411da51c9218bb225d85515c02a5f246e60402
Steve Roehling <sroehling@resultra.com>
2013-09-12 10:09:09 -0700

    WIP implementation of background results generation - see details below.
    * Use the NSNotificationCenter to update to update the views which depend on
      simulation progress to update their results/display.
    * Launch the simulator in the background when the app first starts or the
      user switches to a new plan/budget.
    * Launch the simulator in the background when a managed object change
      occurs in the main DataModelController (NSManagedObjectContext).
    * Remove the code from the ResultsListFormInfoCreator and YearValXYPlotResultsViewController
      to manually execute the simulator when the view was shown and was out of date
      (since the simulator is now run in the background).
    * Migrate some code in EndOfYearDigest.m to use thread-safe date code.

---
+++ Retirement Simulator/ResultsListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsListTableViewController.h
+//  FutureBudget
+//
+//  Created by Steve Roehling on 9/11/13.
+//
+//
+
+#import "GenericFieldBasedTableViewController.h"
+
+@interface ResultsListTableViewController : GenericFieldBasedTableViewController
+
+@end
---
+++ Retirement Simulator/ResultsListTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResultsListTableViewController.m
+//  FutureBudget
+//
+//  Created by Steve Roehling on 9/11/13.
+//
+//
+
+#import "ResultsListTableViewController.h"
+
+@interface ResultsListTableViewController ()
+
+@end
+
+@implementation ResultsListTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
